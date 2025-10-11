58a7236b45b82e6c16dc744e0f12b631d6b58132
elvinsadikhov <elvin.279.6s@gmail.com>
2025-10-11 20:34:52 +0400

    fix: fixed upload part response data bug

--- Domain/obj/Debug/net9.0/Domain.AssemblyInfo.cs
+++ Domain/obj/Debug/net9.0/Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4dca4bd510f1b7fe7abeb93648eb73b4409e6a29")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+83b1c90d5469f19ba5dd95ef1c74b9790aa26ac8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
